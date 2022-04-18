44a471a2d195790240abfa8357131913e85b0b38
FazleRahmanEvan <evanshakib26@gmail.com>
2022-04-18 21:18:30 +0600

    checkout commit

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import './ServiceDetail'
 
 const ServiceDetail = () => {
     const {serviceId} =useParams();
     return (
-        <div>
-            <h1>Welcome To Detail:{serviceId}</h1>
+        <div className='service-detail mb-5'>
+            <h1 className=' mt-5 mb-5'>Welcome To Detail:{serviceId}</h1>
+            <div className='checkout'>
+            <Link to ="/login">
+                <button className=' mb-5 btn btn-primary'>Proceed Checkout</button>
+            </Link>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,19 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import heart from '../../../images/heart.png'
 const Header = () => {
+    const[user] = useAuthState(auth)
+
+    const handleSignOut =() => {
+        signOut(auth);
+    }
+
+
+
     return (
         <>
 
@@ -27,9 +38,17 @@ const Header = () => {
                         </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="/about">About</Nav.Link>
-                            <Nav.Link as={Link} to="login">
+                            
+                            {   
+                              
+                                  user ?
+                                  <button className='btn btn-primary text-white' onClick={handleSignOut}>Sign Out</button>
+                                  : 
+                                <Nav.Link as={Link} to="login">
                                Login 
                             </Nav.Link>
+                            }
+                            
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
