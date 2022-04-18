fdb83c4d2ac22d5d65bb6ab9a3bb0a48b83a2473
FazleRahmanEvan <evanshakib26@gmail.com>
2022-04-18 06:49:12 +0600

    slider commit

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Services from '../Services/Services';
+import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
-        <div>
+        <div className='m-5'>
+            <Banner></Banner>
             <Services></Services>
         </div>
     );
