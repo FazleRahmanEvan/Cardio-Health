8696175211a262b269c34af14ca455e162c2ce04
FazleRahmanEvan <evanshakib26@gmail.com>
2022-04-18 22:05:45 +0600

    error handling commit

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,8 +1,29 @@
 import React from 'react'; 
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import google from '../../../images/google.png'
 import './SocialLogin.css'
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+    
+    if (error) {
+       errorElement = 
+         <div>
+            <p className='text-danger'>Error: {error.message}</p>
+          </div>
+        
+      }
+
+      if(user){
+       navigate('/home');
+      }
+
+
+
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -10,8 +31,11 @@ const SocialLogin = () => {
                 <p className='mt-2 px-2'>or</p>
                 <div style={{height:'1px'}} className='bg-primary w-50' ></div>
             </div>
+            {errorElement}
             <div>
-                <button className=' google-button w-50'>
+                <button 
+                onClick={() => signInWithGoogle()}
+                className=' google-button w-50'>
                     <img className='google' src={google} alt="" />
                    <span className='text-white px-2'> Google Sign In</span></button>
             </div>
