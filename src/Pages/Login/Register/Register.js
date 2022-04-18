dbb6beb0402110e4b679fefe4bd19236badcaa4f
FazleRahmanEvan <evanshakib26@gmail.com>
2022-04-18 20:32:14 +0600

    require auth commit

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './Register.css'
+import auth from '../../../firebase.init';
+
 
 const Register = () => {
+    
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
     const navigate = useNavigate();
     const navigateLogin = () =>{
         navigate('/login');
     }
+    if(user){
+        navigate('/home');
+    }
     const handleRegister = event => {
         event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        createUserWithEmailAndPassword(email,password);
+
+
     }
     return (
         <div className='register-form'>
@@ -19,7 +40,7 @@ const Register = () => {
                 <input type="email" name="email" id="" placeholder='Email Address' required />
                 
                 <input type="password" name="password" id="" placeholder='Password' required />
-                <input type="submit" value="Register" />
+                <input className='register-button' type="submit" value="Register" />
             </form>
             <p>Already have an account? <Link to="/login" className='text-success pe-auto text-decoration-none ' onClick={navigateLogin}>Please Login</Link></p>
         </div>
